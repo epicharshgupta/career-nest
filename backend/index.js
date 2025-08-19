920a5543a42ab3a5fe9c7d187c9cdd03fa439d1a
epicharshgupta <harshitguptapranjul@gmail.com>
2025-08-19 22:37:41 +0530

    cors set

--- backend/index.js
+++ backend/index.js
@@ -10,40 +10,40 @@ import userRouter from './Routes/Userroute.js';
 import companyRouter from './Routes/CompanyRoutes.js';
 import jobRouter from './Routes/jobRoute.js';
 import applicationRouter from './Routes/applicationRoute.js';
-const app=express();
+const app = express();
 
 //middleware
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-const corsoptions={
-    origin: 'http://localhost:5173', // Specify the correct origin
-    credentials:true
+const corsoptions = {
+    origin: ['http://localhost:5173', "https://career-nest-steel.vercel.app"], // Specify the correct origin
+    credentials: true
 }
 app.use(cors(corsoptions));
 
 
 //api
-app.use('/api/v1/user',userRouter)
-app.use('/api/v1/company',companyRouter)
-app.use('/api/v1/job',jobRouter)
-app.use('/api/v1/application',applicationRouter)
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/company', companyRouter)
+app.use('/api/v1/job', jobRouter)
+app.use('/api/v1/application', applicationRouter)
 
 
 
 
 //routes
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     return res.status(200).json({
-        message:"this is job portal app",
-        success:true
+        message: "this is job portal app",
+        success: true
     })
 })
 
 
 const port = 5000
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running at port ${port}`);
     connectDb();
 })
